b91a806c0ebf8bd7e8011ab4ddf02885d565ded9
Muhammad Sharjeel Imran <121516572+SHARJEEL-QURESHI-2021@users.noreply.github.com>
2023-06-14 19:28:40 +0500

    Calender.html

---
+++ app.js
@@ -0,0 +1,297 @@
+let calender = new Date();
+
+let td1 = document.getElementById("td1");
+let td2 = document.getElementById("td2");
+let td3 = document.getElementById("td3");
+let td4 = document.getElementById("td4");
+let td5 = document.getElementById("td5");
+let td6 = document.getElementById("td6");
+let td7 = document.getElementById("td7");
+let td8 = document.getElementById("td8");
+let td9 = document.getElementById("td9");
+let td10 = document.getElementById("td10");
+let td11 = document.getElementById("td11");
+let td12 = document.getElementById("td12");
+let td13 = document.getElementById("td13");
+let td14 = document.getElementById("td14");
+let td15 = document.getElementById("td15");
+let td16 = document.getElementById("td16");
+let td17 = document.getElementById("td17");
+let td18 = document.getElementById("td18");
+let td19 = document.getElementById("td19");
+let td20 = document.getElementById("td20");
+let td21 = document.getElementById("td21");
+let td22 = document.getElementById("td22");
+let td23 = document.getElementById("td23");
+let td24 = document.getElementById("td24");
+let td25 = document.getElementById("td25");
+let td26 = document.getElementById("td26");
+let td27 = document.getElementById("td27");
+let td28 = document.getElementById("td28");
+let td29 = document.getElementById("td29");
+let td30 = document.getElementById("td30");
+let td31 = document.getElementById("td31");
+
+let mArr = ["January", "February", "March", "April", "May", "June", "July","August","September", "October", "November", "December"];
+let dArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+ 
+let month = calender.getMonth();
+let day = calender.getDay();
+
+let m = document.getElementById("month").innerText = mArr[month];
+let d = document.getElementById("day").innerText = dArr[day];
+let y = document.getElementById("year").innerText = calender.getFullYear();
+
+let date = calender.getDate();
+
+if(date == 1){
+    td1.style.color = "white";
+    td1.style.backgroundColor = "rgb(255, 0, 0)";
+    td1.style.fontWeight = "bold";
+    td1.style.fontSize = "28px";
+    td1.style.border = "2px solid rgb(0, 0, 0)";
+    td1.style.textAlign = "center";
+}
+else if(date == 2){
+    td2.style.color = "white";
+    td2.style.backgroundColor = "rgb(255, 0, 0)";
+    td2.style.fontWeight = "bold";
+    td2.style.fontSize = "28px";
+    td2.style.border = "2px solid rgb(0, 0, 0)";
+    td2.style.textAlign = "center";
+}
+else if(date == 3){
+    td3.style.color = "white";
+    td3.style.backgroundColor = "rgb(255, 0, 0)";
+    td3.style.fontWeight = "bold";
+    td3.style.fontSize = "28px";
+    td3.style.border = "2px solid rgb(0, 0, 0)";
+    td3.style.textAlign = "center";
+}
+else if(date == 4){
+    td4.style.color = "white";
+    td4.style.backgroundColor = "rgb(255, 0, 0)";
+    td4.style.fontWeight = "bold";
+    td4.style.fontSize = "28px";
+    td4.style.border = "2px solid rgb(0, 0, 0)";
+    td4.style.textAlign = "center";
+}
+else if(date == 5){
+    td5.style.color = "white";
+    td5.style.backgroundColor = "rgb(255, 0, 0)";
+    td5.style.fontWeight = "bold";
+    td5.style.fontSize = "28px";
+    td5.style.border = "2px solid rgb(0, 0, 0)";
+    td5.style.textAlign = "center";
+}
+else if(date == 6){
+    td6.style.color = "white";
+    td6.style.backgroundColor = "rgb(255, 0, 0)";
+    td6.style.fontWeight = "bold";
+    td6.style.fontSize = "28px";
+    td6.style.border = "2px solid rgb(0, 0, 0)";
+    td6.style.textAlign = "center";
+}
+else if(date == 7){
+    td7.style.color = "white";
+    td7.style.backgroundColor = "rgb(255, 0, 0)";
+    td7.style.fontWeight = "bold";
+    td7.style.fontSize = "28px";
+    td7.style.border = "2px solid rgb(0, 0, 0)";
+    td7.style.textAlign = "center";
+}
+else if(date == 8){
+    td8.style.color = "white";
+    td8.style.backgroundColor = "rgb(255, 0, 0)";
+    td8.style.fontWeight = "bold";
+    td8.style.fontSize = "28px";
+    td8.style.border = "2px solid rgb(0, 0, 0)";
+    td8.style.textAlign = "center";
+}
+else if(date == 9){
+    td9.style.color = "white";
+    td9.style.backgroundColor = "rgb(255, 0, 0)";
+    td9.style.fontWeight = "bold";
+    td9.style.fontSize = "28px";
+    td9.style.border = "2px solid rgb(0, 0, 0)";
+    td9.style.textAlign = "center";
+}
+else if(date == 10){
+    td10.style.color = "white";
+    td10.style.backgroundColor = "rgb(255, 0, 0)";
+    td10.style.fontWeight = "bold";
+    td10.style.fontSize = "28px";
+    td10.style.border = "2px solid rgb(0, 0, 0)";
+    td10.style.textAlign = "center";
+}
+else if(date == 11){
+    td11.style.color = "white";
+    td11.style.backgroundColor = "rgb(255, 0, 0)";
+    td11.style.fontWeight = "bold";
+    td11.style.fontSize = "28px";
+    td11.style.border = "2px solid rgb(0, 0, 0)";
+    td11.style.textAlign = "center";
+}
+else if(date == 12){
+    td12.style.color = "white";
+    td12.style.backgroundColor = "rgb(255, 0, 0)";
+    td12.style.fontWeight = "bold";
+    td12.style.fontSize = "28px";
+    td12.style.border = "2px solid rgb(0, 0, 0)";
+    td12.style.textAlign = "center";
+}
+else if(date == 13){
+    td13.style.color = "white";
+    td13.style.backgroundColor = "rgb(255, 0, 0)";
+    td13.style.fontWeight = "bold";
+    td13.style.fontSize = "28px";
+    td13.style.border = "2px solid rgb(0, 0, 0)";
+    td13.style.textAlign = "center";
+}
+else if(date == 14){
+    td14.style.color = "white";
+    td14.style.backgroundColor = "rgb(255, 0, 0)";
+    td14.style.fontWeight = "bold";
+    td14.style.fontSize = "28px";
+    td14.style.border = "2px solid rgb(0, 0, 0)";
+    td14.style.textAlign = "center";
+}
+else if(date == 15){
+    td15.style.color = "white";
+    td15.style.backgroundColor = "rgb(255, 0, 0)";
+    td15.style.fontWeight = "bold";
+    td15.style.fontSize = "28px";
+    td15.style.border = "2px solid rgb(0, 0, 0)";
+    td15.style.textAlign = "center";
+}
+else if(date == 16){
+    td16.style.color = "white";
+    td16.style.backgroundColor = "rgb(255, 0, 0)";
+    td16.style.fontWeight = "bold";
+    td16.style.fontSize = "28px";
+    td16.style.border = "2px solid rgb(0, 0, 0)";
+    td16.style.textAlign = "center";
+}
+else if(date == 17){
+    td17.style.color = "white";
+    td17.style.backgroundColor = "rgb(255, 0, 0)";
+    td17.style.fontWeight = "bold";
+    td17.style.fontSize = "28px";
+    td17.style.border = "2px solid rgb(0, 0, 0)";
+    td17.style.textAlign = "center";
+}
+else if(date == 18){
+    td18.style.color = "white";
+    td18.style.backgroundColor = "rgb(255, 0, 0)";
+    td18.style.fontWeight = "bold";
+    td18.style.fontSize = "28px";
+    td18.style.border = "2px solid rgb(0, 0, 0)";
+    td18.style.textAlign = "center";
+}
+else if(date == 19){
+    td19.style.color = "white";
+    td19.style.backgroundColor = "rgb(255, 0, 0)";
+    td19.style.fontWeight = "bold";
+    td19.style.fontSize = "28px";
+    td19.style.border = "2px solid rgb(0, 0, 0)";
+    td19.style.textAlign = "center";
+}
+else if(date == 20){
+    td20.style.color = "white";
+    td20.style.backgroundColor = "rgb(255, 0, 0)";
+    td20.style.fontWeight = "bold";
+    td20.style.fontSize = "28px";
+    td20.style.border = "2px solid rgb(0, 0, 0)";
+    td20.style.textAlign = "center";
+}
+else if(date == 21){
+    td21.style.color = "white";
+    td21.style.backgroundColor = "rgb(255, 0, 0)";
+    td21.style.fontWeight = "bold";
+    td21.style.fontSize = "28px";
+    td21.style.border = "2px solid rgb(0, 0, 0)";
+    td21.style.textAlign = "center";
+}
+else if(date == 22){
+    td22.style.color = "white";
+    td22.style.backgroundColor = "rgb(255, 0, 0)";
+    td22.style.fontWeight = "bold";
+    td22.style.fontSize = "28px";
+    td22.style.border = "2px solid rgb(0, 0, 0)";
+    td22.style.textAlign = "center";
+}
+else if(date == 23){
+    td23.style.color = "white";
+    td23.style.backgroundColor = "rgb(255, 0, 0)";
+    td23.style.fontWeight = "bold";
+    td23.style.fontSize = "28px";
+    td23.style.border = "2px solid rgb(0, 0, 0)";
+    td23.style.textAlign = "center";
+}
+else if(date == 24){
+    td24.style.color = "white";
+    td24.style.backgroundColor = "rgb(255, 0, 0)";
+    td24.style.fontWeight = "bold";
+    td24.style.fontSize = "28px";
+    td24.style.border = "2px solid rgb(0, 0, 0)";
+    td24.style.textAlign = "center";
+}
+else if(date == 25){
+    td25.style.color = "white";
+    td25.style.backgroundColor = "rgb(255, 0, 0)";
+    td25.style.fontWeight = "bold";
+    td25.style.fontSize = "28px";
+    td25.style.border = "2px solid rgb(0, 0, 0)";
+    td25.style.textAlign = "center";
+}
+else if(date == 26){
+    td26.style.color = "white";
+    td26.style.backgroundColor = "rgb(255, 0, 0)";
+    td26.style.fontWeight = "bold";
+    td26.style.fontSize = "28px";
+    td26.style.border = "2px solid rgb(0, 0, 0)";
+    td26.style.textAlign = "center";
+}
+else if(date == 27){
+    td27.style.color = "white";
+    td27.style.backgroundColor = "rgb(255, 0, 0)";
+    td27.style.fontWeight = "bold";
+    td27.style.fontSize = "28px";
+    td27.style.border = "2px solid rgb(0, 0, 0)";
+    td27.style.textAlign = "center";
+}
+else if(date == 28){
+    td28.style.color = "white";
+    td28.style.backgroundColor = "rgb(255, 0, 0)";
+    td28.style.fontWeight = "bold";
+    td28.style.fontSize = "28px";
+    td28.style.border = "2px solid rgb(0, 0, 0)";
+    td28.style.textAlign = "center";
+}
+else if(date == 29){
+    td29.style.color = "white";
+    td29.style.backgroundColor = "rgb(255, 0, 0)";
+    td29.style.fontWeight = "bold";
+    td29.style.fontSize = "28px";
+    td29.style.border = "2px solid rgb(0, 0, 0)";
+    td29.style.textAlign = "center";
+}
+else if(date == 30){
+    td30.style.color = "white";
+    td30.style.backgroundColor = "rgb(255, 0, 0)";
+    td30.style.fontWeight = "bold";
+    td30.style.fontSize = "28px";
+    td30.style.border = "2px solid rgb(0, 0, 0)";
+    td30.style.textAlign = "center";
+}
+else if(date == 31){
+    td31.style.color = "white";
+    td31.style.backgroundColor = "rgb(255, 0, 0)";
+    td31.style.fontWeight = "bold";
+    td31.style.fontSize = "28px";
+    td31.style.border = "2px solid rgb(0, 0, 0)";
+    td31.style.textAlign = "center";
+}
+else{
+
+}
